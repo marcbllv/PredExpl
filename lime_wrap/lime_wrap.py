b878d0223834204a3a7f442325f4806e40360127
Marc Beillevaire <marc.beillevaire@gmail.com>
2016-11-08 14:01:55 +0100

    Fixes for classification

--- lime_wrap/lime_wrap.py
+++ lime_wrap/lime_wrap.py
@@ -5,10 +5,13 @@ from lime.lime_tabular import LimeTabularExplainer
 
 class LimeWrapper:
 
-    def __init__(self, train, model, target_column, categorical=[]):
+    def __init__(self, train, model, target_column, categorical=[], kernel_width=None):
+        if not isinstance(train, pd.DataFrame):
+            raise Exception('Use a pandas DataFrame to store the train set')
+
         # Handling target
         if target_column not in train.columns:
-            raise('ERROR: target column not in train set.')
+            raise Exception('Target column not in train set.')
         target = train[target_column]
         train_x = train.drop(target_column, axis=1)
 
@@ -34,21 +37,23 @@ class LimeWrapper:
         if pred_type == 'classification':
             class_names = np.unique(target)
             self.labels = [1]
-            self.predict_fn = lambda x: model.predict_proba(x)[1].reshape(-1, 1)
+            self.predict_fn = lambda x: model.predict_proba(x)
         else:
             class_names = ['lower', 'higher']
             self.labels = [0]
             self.predict_fn = lambda x: model.predict(x).reshape(-1, 1)
 
-        # saving the model
-
         # setting up the explainer
         self.explainer = LimeTabularExplainer(train_x.values,
                 feature_names=features, class_names=class_names,
                 categorical_features=cat_idx,
-                categorical_names=categorical_names, kernel_width=None)
+                categorical_names=categorical_names, kernel_width=kernel_width,
+                verbose=True)
 
     def explain(self, instance, num_features=10, num_samples=1000, labels=None, show=True):     
+        if not isinstance(instance, pd.Series):
+            raise Exception('Use a pandas Serie to store the train set')
+
         exp = self.explainer.explain_instance(instance.values, self.predict_fn,
                 num_features=num_features, num_samples=num_samples,
                 labels=self.labels)
