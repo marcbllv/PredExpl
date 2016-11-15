6c12ce4432aa13103f148cd0a265ea514415dcfe
Marc Beillevaire <marc.beillevaire@gmail.com>
2016-11-15 13:15:06 +0100

    fix...

--- lime_wrap/lime_wrap.py
+++ lime_wrap/lime_wrap.py
@@ -10,7 +10,7 @@ class LimeWrapper:
         if not isinstance(train, pd.DataFrame):
             raise Exception('Use a pandas DataFrame to store the train set')
 
-        train.fill_na(0.0)
+        train.fillna(0.0)
 
         # Handling target
         if target_column not in train.columns:
